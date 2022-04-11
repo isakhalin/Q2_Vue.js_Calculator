f300c87395c6c9bf69582e8af9a725d825f5b52c
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-04-11 17:11:54 +1100

    edit

--- src/test/TestVuex.test.js
+++ src/test/TestVuex.test.js
@@ -1,19 +1,21 @@
 import VuexComp from "../components/VuexComp.vue";
 import {shallowMount, createLocalVue} from "@vue/test-utils";
+//shallowMount - Монтирует компонент не целиком, а с заглушками вместо вложенных компонентов
+//createLocalVue - Функция, которая создает локальный инстанс
 import Vuex from 'vuex';
 
-const localVue = createLocalVue();
-localVue.use(Vuex)
+const localVue = createLocalVue(); //Создаем локальный инстанс и записываем его в localVue
+localVue.use(Vuex); //Использовать Vuex в локальном инстансе.
 
 describe('Test store', () => {
     let actions;
     let store;
 
-    beforeEach(() => {
-        actions = {
-            addData: jest.fn()
-        }
-
+    beforeEach(() => {  //Хук, который выполняет действия перед каждым тестом.
+        actions = {         //В данном случае в хуке очищается хранилище.
+            addData: jest.fn()  //Функция МОК. которая смотрит за addData и как только addData
+        }                      //будет вызвана в тесте, функция буде помечена как вызов и мы сможем
+                                //это проверить.
         store = new Vuex.Store({
             actions,
             state: {
@@ -35,6 +37,6 @@ describe('Test store', () => {
 
         const btn = wrapper.find('button[name="save"]');
         btn.trigger('click');
-        expect(actions.addData).toHaveBeenCalled()
+        expect(actions.addData).toHaveBeenCalled() //Метод toHaveBeenCalled проверяет была ли вызвана функция
     })
 });
